36e6ad393353041cfc03f3d400344fe99784c1d4
dimseres <dpaslamuskas@gmail.com>
2023-04-30 16:50:49 +0600

    add redis

---
+++ authorization/internal/database/redis.go
@@ -0,0 +1,25 @@
+package database
+
+import "github.com/redis/go-redis/v9"
+
+type RedisConnectionConfig struct {
+	Host     string
+	Password string
+	DB       int
+}
+
+var redisConn *redis.Client
+
+func InitRedisConnection(config *RedisConnectionConfig) *redis.Client {
+	redisConn := redis.NewClient(&redis.Options{
+		Addr:     config.Host,
+		Password: config.Password, // no password set
+		DB:       config.DB,       // use default DB
+	})
+
+	return redisConn
+}
+
+func GetRedisConnection() *redis.Client {
+	return redisConn
+}
