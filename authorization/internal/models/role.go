8715bc6f920c350c6d34ec3e95d4fc483de7d9ee
dimseres <dpaslamuskas@gmail.com>
2023-05-04 17:35:16 +0600

    hide properties

--- authorization/internal/models/role.go
+++ authorization/internal/models/role.go
@@ -6,9 +6,9 @@ type Role struct {
 	Name           string          `gorm:"not null;index;uniqueIndex:unique_index" json:"title"`
 	Slug           string          `gorm:"not null;index;unique;uniqueIndex:unique_index" json:"slug"`
 	Description    *string         `gorm:"type:text" json:"description"`
-	IsSystem       bool            `gorm:"default:false" json:"is_system"`
-	OrganizationId *uint           `gorm:"uniqueIndex:unique_index"`
-	Organizations  *[]Organization `gorm:"many2many:memberships" json:"organization"`
+	IsSystem       bool            `gorm:"default:false" json:"is_system" json:"-"`
+	OrganizationId *uint           `gorm:"uniqueIndex:unique_index" json:"-"`
+	Organizations  *[]Organization `gorm:"many2many:memberships" json:"-"`
 	Users          *[]User         `gorm:"many2many:memberships" json:"user"`
 	//Permissions    *[]Permissions  `gorm:"many2many:organizations;foreignKey:name;joinForeignKey:domain;References:v0;joinReferences:v3"`
 
