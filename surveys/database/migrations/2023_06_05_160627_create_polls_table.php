ec058a6892d5291fcab9fbcaa35e6d10d930f773
dimseres <dpaslamuskas@gmail.com>
2023-06-06 00:59:29 +0600

    fix

--- surveys/database/migrations/2023_06_05_160627_create_polls_table.php
+++ surveys/database/migrations/2023_06_05_160627_create_polls_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('polls', function (Blueprint $table) {
             $table->id();
             $table->foreignId('creator_id')->constrained('users');
-            $table->foreignId('organization_id')->constrained('organization');
+            $table->foreignId('organization_id')->constrained('organizations');
             $table->string('title');
             $table->text('description')->nullable();
             $table->string('slug');
