4de1f39ae797e4197ddbdf929425c487d9d4521b
dimseres <dpaslamuskas@gmail.com>
2023-06-06 00:59:35 +0600

    jwt auth

--- surveys/app/Providers/AuthServiceProvider.php
+++ surveys/app/Providers/AuthServiceProvider.php
@@ -3,7 +3,9 @@
 namespace App\Providers;
 
 // use Illuminate\Support\Facades\Gate;
+
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+use Illuminate\Support\Facades\Auth;
 
 class AuthServiceProvider extends ServiceProvider
 {
@@ -21,6 +23,20 @@ class AuthServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        Auth::viaRequest('jwt', function (Request $request) {
+            try {
+                $guid = $request->header('X-REQUEST-ID');
+                if (!$request->bearerToken() && !$guid) {
+                    throw new \Exception("token missed");
+                }
+                $tokenPayload = JWT::decode($request->bearerToken(), new Key($guid.env("GATEWAY_KEY"), 'HS256'));
+                $user = User::query()->find($tokenPayload->data->user_id);
+                return $user;
+
+            } catch (\Exception $exception) {
+                Log::error($exception);
+                return null;
+            }
+        });
     }
 }
